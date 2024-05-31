cfeda6023face40fcdb89d42a4e490602288e245
PORTODIT <adhityapmp@gmail.com>
2024-06-01 01:44:11 +0700

    add typography

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,10 +59,4 @@ dependencies {
     implementation (libs.androidx.constraintlayout)
     testImplementation (libs.junit)
     androidTestImplementation (libs.androidx.espresso.core)
-    implementation("androidx.compose.ui:ui:1.0.0")
-    implementation("androidx.compose.material:material:1.0.0")
-    implementation("androidx.compose.ui:ui-tooling:1.0.0")
-    implementation("androidx.compose.runtime:runtime-livedata:1.0.0")
-    implementation("androidx.activity:activity-compose:1.3.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
 }
