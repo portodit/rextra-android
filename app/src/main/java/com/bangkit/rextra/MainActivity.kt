039c0c0510a6263a0e79f0ed01fd0eb9c2ac418d
PORTODIT <adhityapmp@gmail.com>
2024-06-20 01:42:23 +0700

    merge onboarding login register rextra

--- app/src/main/java/com/bangkit/rextra/MainActivity.kt
+++ app/src/main/java/com/bangkit/rextra/MainActivity.kt
@@ -7,8 +7,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.bangkit.rextra.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,7 +26,29 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.homeToolbar)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.PortofolioFragment,
+                R.id.PencarianFragment,
+                R.id.KegiatanFragment,
+                R.id.ProfilFragment
+            )
+        )
+
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -30,5 +59,11 @@ class MainActivity : AppCompatActivity() {
     private fun isOnboardingCompleted(): Boolean {
         return getSharedPreferences("OnboardingPrefs", Context.MODE_PRIVATE)
             .getBoolean("isCompleted", false)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
